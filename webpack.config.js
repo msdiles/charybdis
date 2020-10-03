5c5a4efcc0362c05ab89fde0f0c0f012f843a83c
Dmitrii Leshchuk <msdiles@gmail.com>
2020-10-03 19:44:57 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path")
+const HTMLPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "/dist"),
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [{loader: "ts-loader"}]
+            },
+        ]
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: "src/index.html",
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true
+            }
+        })
+    ],
+    devServer: {
+        port: 8000,
+        contentBase: "dist",
+        compress: true,
+        open: true,
+        watchContentBase: true,
+        hot:true
+    }
+}
